cae2b4b146110d26fab4571020313308da318f0b
shubham <shubhamranga@gmail.com>
2021-05-22 01:16:30 +0530

    added scripts

--- client/app.js
+++ client/app.js
@@ -1,2 +1,19 @@
-const form = document.moodForm;
-const emotionNote = document.moodForm.emotionNote;
+window.onload = function () {
+  const form = document.moodForm;
+  const emotionNote = document.moodForm.emotionNote;
+  const submit = document.getElementsByClassName("submit_button");
+
+  submit.onclick = () => {
+    console.log(emotionNote.value);
+    chrome.storage.sync.set(
+      {
+        emotionNote: emotionNote.value,
+      },
+      function () {
+        console.log("Value is set to " + emotionNote);
+      }
+    );
+  };
+
+  console.log(emotionNote.value);
+};
