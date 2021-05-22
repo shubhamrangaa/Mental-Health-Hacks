bd31196d0e6cfff84ebb6a05f42cc40b9728248f
shubham <shubhamranga@gmail.com>
2021-05-23 00:32:50 +0530

    broken fix

--- client/Calendar.js
+++ client/Calendar.js
@@ -1,16 +1,30 @@
+let date = [""];
+let data_display = "";
+let emoArray = [""];
+
 window.onload = () => {
-  const date = document.querySelectorAll(".grid-item");
-  const date_display = document.querySelector(".EMO_NOTE_CHILD");
-  let emoArray = [""];
+  date = document.querySelectorAll(".grid-item");
+  date_display = document.querySelector(".EMO_NOTE_CHILD");
 
   //   getting notes in emoArray
   chrome.storage.sync.get(["emotion"], function (result) {
     if (result.emotion) {
       emoArray = result.emotion;
     }
+    console.log(emoArray, date, data_display);
   });
 
-  date[1].onclick = () => {
-    date_display.textContent = emoArray[1];
-  };
+  //   date[1].onclick = () => {
+  //     date_display.textContent = emoArray[1];
+  //   };
 };
+
+if (date[1]) {
+  for (i = 2; i < 30; i++) {
+    console.log(emoArray[i]);
+    console.log("hel");
+    date[i].onclick = () => {
+      date_display.textContent = emoArray[i];
+    };
+  }
+}
