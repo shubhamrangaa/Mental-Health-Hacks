dfba50eeebb715734676d08a019c52c58315dd62
shubham <shubhamranga@gmail.com>
2021-05-22 23:49:39 +0530

    functional history page

--- client/Calendar.js
+++ client/Calendar.js
@@ -6,24 +6,11 @@ window.onload = () => {
   //   getting notes in emoArray
   chrome.storage.sync.get(["emotion"], function (result) {
     if (result.emotion) {
-      emoArray.push(result.emotion);
+      emoArray = result.emotion;
     }
   });
-  //   test
-  for (i = 0; i < 10; i++) {
-    console.log(date[i].textContent);
-    // console.log(emoArray[i]);
-  }
-  //
-  //   for (i = 0; i < 30; i++) {
-  //     console.log(emoArray[i]);
-  //     date[i].onClick = () => {
-  //       date_display.textContent = emoArray[i];
-  //     };
-  //   }
-  date[0].onclick = () => {
-    console.log("jel");
-    console.log(emoArray[6]);
+
+  date[1].onclick = () => {
     date_display.textContent = emoArray[1];
   };
 };
--- client/read.js
+++ client/read.js
@@ -2,6 +2,15 @@ window.onload = () => {
   const section = document.getElementById("logContainer");
   chrome.storage.sync.get(["emotion"], function (result) {
     const emoArray = result.emotion;
-    section.innerHTML = emoArray;
+    for (i = 1; i < emoArray.length; i++) {
+      var tag = document.createElement("p");
+      var text = document.createTextNode(emoArray[i]);
+      tag.appendChild(text);
+      var tagDate = document.createElement("h4");
+      var dateText = document.createTextNode(`${i} May 2021`);
+      tagDate.appendChild(dateText);
+      section.appendChild(tagDate);
+      section.appendChild(tag);
+    }
   });
 };
