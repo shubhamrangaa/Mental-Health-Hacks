80a4994a6155d43e217a149080492f134e7094af
shubham <shubhamranga@gmail.com>
2021-05-23 14:50:45 +0530

    functional calendar

--- client/Calendar.js
+++ client/Calendar.js
@@ -2,29 +2,31 @@ let date = [""];
 let data_display = "";
 let emoArray = [""];
 
-window.onload = () => {
-  date = document.querySelectorAll(".grid-item");
-  date_display = document.querySelector(".EMO_NOTE_CHILD");
+date = document.querySelectorAll(".grid-item");
+date_display = document.querySelector(".EMO_NOTE_CHILD");
 
-  //   getting notes in emoArray
-  chrome.storage.sync.get(["emotion"], function (result) {
-    if (result.emotion) {
-      emoArray = result.emotion;
-    }
-    console.log(emoArray, date, data_display);
-  });
-
-  //   date[1].onclick = () => {
-  //     date_display.textContent = emoArray[1];
-  //   };
-};
-
-if (date[1]) {
-  for (i = 2; i < 30; i++) {
-    console.log(emoArray[i]);
-    console.log("hel");
-    date[i].onclick = () => {
-      date_display.textContent = emoArray[i];
-    };
+//   getting notes in emoArray
+chrome.storage.sync.get(["emotion"], function (result) {
+  if (result.emotion) {
+    emoArray = result.emotion;
   }
-}
+  console.log(emoArray, date, data_display);
+});
+
+document.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.matches(".grid-item")) {
+      // Run your code to open a modal
+      console.log("voila");
+      console.log(event.target.textContent);
+      const day = event.target.textContent;
+      if (emoArray[day]) {
+        date_display.textContent = emoArray[day];
+      } else {
+        date_display.textContent = "No Entries found";
+      }
+    }
+  },
+  false
+);
